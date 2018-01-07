7c17d70f88346097570336dcce3cdb67709da24c
Yuka <3702771+ykmsd@users.noreply.github.com>
2018-01-07 23:47:06 +0900

    stylying header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import { Layout, Menu } from 'antd';
+import { Button } from 'antd';
 import './header.css';
-
-const { Header: HeaderContainer } = Layout;
 
 const Header = () => {
   return (
-    <Layout className="header">
-      <HeaderContainer>
-        <Menu theme="dark" mode="horizontal">
-          <Menu.Item key="1">Andres</Menu.Item>
-          <Menu.Item key="2">Logout</Menu.Item>
-        </Menu>
-      </HeaderContainer>
-    </Layout>
+    <div className="header">
+      <h1>Hemsut</h1>
+      <ul>
+        <li>Andres</li>
+        <li><Button>Logout</Button></li>
+      </ul>
+    </div>
   );
 };
 
